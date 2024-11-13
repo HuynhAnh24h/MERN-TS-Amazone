6137de369f09211cc193d8f983f0e1ac301b489f
HuynhAnh8877 <anhdev24h@gmail.com>
2024-11-13 09:53:49 +0700

    Create React TS with Vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <div>
+      <header>
+          TS amazone React
+      </header>
+      <main>
+          TS main React amazone
+      </main>
+      <footer>
+        All rise 
+      </footer>
+    </div>
+  )
+}
+
+export default App
